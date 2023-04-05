3e46340906153297cfe84b5815de84d608756f28
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-04-05 14:11:58 +0200

    Non so se ho capito l'esercizio

--- js/style.js
+++ js/style.js
@@ -4,4 +4,13 @@
 multi
 linea
 */
-document.getElementById('Titolo').style.color = "#ff0000";
+document.getElementById('Pass').style.color = "#ff0000";
+let user_name = prompt ("Come ti chiami?")
+
+let user_surname = prompt ("Qual e' il tuo cognome?")
+
+let user_color = prompt ("E il tuo colore preferito?")
+
+let pass = "La tua password è: nome-cognome-colorepreferito-2023"
+
+document.getElementById("Pass").innerHTML = pass;
